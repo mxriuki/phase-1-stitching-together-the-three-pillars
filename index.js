3e18710246a2e6442b7dce640f36e6a168a9c768
mxriuki <mwendav37@gmail.com>
2024-06-27 11:02:18 +0300

    code

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+const testVar = {"extends": [
+    "development"
+  ],
+  "hints": {
+    "meta-viewport": "off",
+    "meta-charset-utf-8": "off"
+  }}
